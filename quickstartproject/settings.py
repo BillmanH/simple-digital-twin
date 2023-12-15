f1a10a26f8f642352ab18362b3bb58b841f83145
billmanh <william.harding@avanade.com>
2023-12-15 15:35:05 -0800

    Rename folders

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello_azure'
+    'simple_twin'
 ]
 
 MIDDLEWARE = [                                                                   
