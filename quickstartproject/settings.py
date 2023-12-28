407dba5dde8647ecee790fe2753d149064adc026
billmanh <william.harding@avanade.com>
2023-12-28 14:58:31 -0800

    added logging

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -130,6 +130,24 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "./logs/app_log.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
